fa7ee04785ab82a4bc2683ee37042ee09a71b8d3
lophiester <gilcharles2022@hotmail.com>
2024-11-16 08:16:53 +0900

    Initial Commit

---
+++ Citysights App/Citysights_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Citysights_AppApp.swift
+//  Citysights App
+//
+//  Created by Charles Yamamoto on 2024/11/16.
+//
+
+import SwiftUI
+
+@main
+struct Citysights_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
